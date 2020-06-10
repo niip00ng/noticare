910d0b9e4f26fae94e8325ffc20630819e9f02ae
이준환 <niip00ng@daum.net>
2020-06-10 21:31:15 +0900

    [DEV] FCM 토큰 생성
    - javaMaxHeapSize 최대 heap 사이즈 설정
    - multiDexEnabled 설정

--- App.tsx
+++ App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import messaging from '@react-native-firebase/messaging';
-import { AppRegistry } from 'react-native';
-import {
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import {AppRegistry} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
+    console.log('Message handled in the background!', remoteMessage);
 });
 
+async function saveTokenToDatabase(token : string) {
+    // Assume user is already signed in Add the token to the users datastore
+    await firestore()
+        .collection('users')
+        .doc('11111')
+        .update({
+            tokens: firestore
+                .FieldValue
+                .arrayUnion(token)
+        });
+}
+
 const App = () => {
-  return (
-    <>
-      <View><Text>초기화면</Text></View>
-    </>
-  );
+    useEffect(() => {
+        // Get the device token
+        messaging()
+            .getToken()
+            .then(token => {
+              console.log(token)
+              
+                return saveTokenToDatabase(token);
+            });
+
+        // Listen to whether the token changes
+        return messaging().onTokenRefresh(token => {
+            saveTokenToDatabase(token);
+        });
+    }, []);
+
+    return (
+        <View>
+          <Text>start</Text>
+        </View>
+    )
 };
 
 export default App;
